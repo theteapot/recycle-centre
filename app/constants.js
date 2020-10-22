fdcfd4f3bcde2a1d2e1d738fdb581d0aa2fb12e0
Taylor Kettle <tin.teapot@gmail.com>
2020-10-22 23:26:34 +1300

    changed SERVER to prod

--- app/constants.js
+++ app/constants.js
@@ -1,3 +1,3 @@
-// export const SERVER =
-//   "http://ec2-13-55-100-110.ap-southeast-2.compute.amazonaws.com:8888";
-export const SERVER = "http://192.168.1.127:5142";
+export const SERVER =
+  "http://ec2-13-55-100-110.ap-southeast-2.compute.amazonaws.com:8888";
+// export const SERVER = "http://192.168.1.127:5142";
