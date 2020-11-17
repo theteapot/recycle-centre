da304085e7cfb2686441a83da4aa314856e146c0
Taylor Kettle <tin.teapot@gmail.com>
2020-11-17 22:40:58 +1300

    changing server back

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,5 +1,5 @@
-./build-docker.sh
-# ~/.ssh/connect-to-recycle-centre-aws
-# docker stop
-# docker image pull theteapot/recycle-centre-server:latest
-# docker run --rm -p 8888:5142 --name recycle-centre-server  --mount source=recycle-centre-volume,target=/data/db theteapot/recycle-centre-server
+~/.ssh/connect-to-recycle-centre-aws << 'EOF'
+sudo docker stop $(sudo docker container ls -q --filter name=recycle-centre-server)
+sudo docker image pull theteapot/recycle-centre-server:latest
+sudo docker run --rm -p 8888:5142 --name recycle-centre-server  --mount source=recycle-centre-volume,target=/data/db theteapot/recycle-centre-server
+EOF
