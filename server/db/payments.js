11d0d75587df49b8da0559967c374f0aa9cd2dc8
Taylor Kettle <tin.teapot@gmail.com>
2020-12-14 00:50:25 +1300

    changed timestamp retrieval

--- server/db/payments.js
+++ server/db/payments.js
@@ -20,24 +20,20 @@ async function getPayments() {
   let result = await (await collection.aggregate([])).toArray();
   result = result.map((record) => ({
     ...record,
-    timestamp: new moment(record.timestamp._d || record.timestamp)
-      .tz("Pacific/Auckland")
-      .toString(),
+    timestamp: new moment(record.timestamp).tz("Pacific/Auckland").toString(),
   }));
   db.close();
   return result;
 }
 
-async function getOrders() {
+async function getOrders(startDate, endDate) {
   const { db, dbo } = await connect();
   const collection = dbo.collection("orders");
   let result = await (await collection.aggregate([])).toArray();
   result = result.map((record) => ({
     ...record,
     total: record.paymentAmount * record.productQuantity,
-    timestamp: new moment(record.timestamp._d || record.timestamp)
-      .tz("Pacific/Auckland")
-      .toString(),
+    timestamp: new moment(record.timestamp).tz("Pacific/Auckland").toString(),
   }));
   db.close();
   return result;
